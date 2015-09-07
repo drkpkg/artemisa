a3b5313ea03cfbc596275c3318b67f2f7b75b59f
Felix Coca <dcoca@fastthrive.com>
2015-09-07 14:46:27 -0400

    Se quita confifuraciones dentro de application.rb por errores en despliegue.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
 
-  Bundler.require(*Rails.groups)
-  Dotenv::Railtie.load
+  #Bundler.require(*Rails.groups)
+  #Dotenv::Railtie.load
 
   protect_from_forgery with: :exception
   before_filter :set_cache_buster
