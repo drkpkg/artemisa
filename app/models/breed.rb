7b067be8665dfbd3c06837b2e75b23b6e43d5e35
drkpkg <daniel.uremix@gmail.com>
2015-09-25 18:33:03 -0400

    Relaciones modificadas.  Cambios para hacer commit: 	modificado: app/models/animal.rb 	modificado: app/models/breed.rb 	modificado: app/models/specie.rb 	modificado: db/migrate/20150922121601_create_animals.rb 	modificado: db/migrate/20150922121623_create_species.rb 	modificado: db/migrate/20150922121629_create_breeds.rb

--- app/models/breed.rb
+++ app/models/breed.rb
@@ -1,3 +1,4 @@
 class Breed < ActiveRecord::Base
+  belongs_to :animal, dependent: destroy
   belongs_to :specie, dependent: destroy
 end
--- db/migrate/20150922121601_create_animals.rb
+++ db/migrate/20150922121601_create_animals.rb
@@ -1,7 +1,7 @@
 class CreateAnimals < ActiveRecord::Migration
   def change
     create_table :animals do |t|
-
+      t.belongs_to :breed, index: true
       t.timestamps
     end
   end
