3ba07824c52aab4a81165bb86f4f505ae08948fb
Felix Coca <dcoca@fastthrive.com>
2015-09-28 13:25:47 -0400

    FIXD: Error en migracion.

--- db/migrate/20150928135713_create_type_employees.rb
+++ db/migrate/20150928135713_create_type_employees.rb
@@ -1,7 +1,7 @@
 class CreateTypeEmployees < ActiveRecord::Migration
   def change
     create_table :type_employees do |t|
-      t.string type_employee
+      t.string :type_employee
       t.timestamps
     end
   end
