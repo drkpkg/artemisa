954adf4946c7401f7a0777929afb4888ca72fcb9
Felix Coca <dcoca@fastthrive.com>
2015-09-28 13:26:46 -0400

    FIXD: Migraciones arregladas.

--- db/migrate/20150922121623_create_species.rb
+++ db/migrate/20150922121623_create_species.rb
@@ -1,7 +1,7 @@
 class CreateSpecies < ActiveRecord::Migration
   def change
     create_table :species do |t|
-      t.string specie_type
+      t.string :specie_type
       t.timestamps
     end
   end
--- db/migrate/20150922121629_create_breeds.rb
+++ db/migrate/20150922121629_create_breeds.rb
@@ -1,7 +1,7 @@
 class CreateBreeds < ActiveRecord::Migration
   def change
     create_table :breeds do |t|
-      t.string breed_type
+      t.string :breed_type
       t.belongs_to :specie, index: true
       t.timestamps
     end
