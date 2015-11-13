2397538407588cc1472a70630d661243033219a7
drkpkg <daniel.uremix@gmail.com>
2015-11-13 17:51:38 -0400

    Se quita raza_id de especie por culpa de Daniel :v

--- db/migrate/20151026024418_create_especies.rb
+++ db/migrate/20151026024418_create_especies.rb
@@ -2,7 +2,6 @@ class CreateEspecies < ActiveRecord::Migration
   def change
     create_table :especies do |t|
       t.string :nombre_especie
-      t.belongs_to :raza, index: true
       t.timestamps
     end
   end
